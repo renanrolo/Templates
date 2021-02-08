845cf8522ec5c3ccb43eb15b00f95f532c7c905a
Renan Rolo <renanrolo@hotmail.com>
2021-02-08 16:17:53 -0400

    Api with swagger and healthcheck

---
+++ Templates/Templates.Api/Startup.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Templates.Api.Extensions;
+
+namespace Templates.Api
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            _configuration = builder.Build();
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen()
+                    .AddControllers();
+
+            services.AddHealthChecks();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger(_configuration)
+                .UseHttpsRedirection()
+                .UseRouting()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                })
+                .UseHealthChecks("/healthcheck");
+        }
+    }
+}
