b5980778dbf8c0eba76212cfc396594d6157be61
Renan Rolo <renanrolo@hotmail.com>
2021-03-03 12:21:55 -0400

    sanitization

--- Templates/Templates.Api/Extensions/SwaggerExtension.cs
+++ Templates/Templates.Api/Extensions/SwaggerExtension.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerUI;
 using System;
--- Templates/Templates.IoC/Exemples/ConfigurationExemple.cs
+++ Templates/Templates.IoC/Exemples/ConfigurationExemple.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Templates.IoC
+﻿namespace Templates.IoC
 {
     public class ConfigurationExemple
     {
